445769569b37cb23c0e6062e2740bbe1eb6807fa
504 <504@DESKTOP-B0KVD9C>
2021-07-12 13:05:39 +0900

    chap02-완료_chap03-진행중

---
+++ Java1/src/chap02/LogicBinary.java
@@ -0,0 +1,20 @@
+package chap02;
+
+public class LogicBinary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0, b = 0;
+		
+		String c;
+		int d = a & b;
+		
+		if (a>=1 || b>=1) {
+			c = "True";
+		} else {
+			c = "False";
+		}
+		System.out.println(c);
+	}
+
+}
---
+++ Java1/src/chap02/VariableExample.java
@@ -0,0 +1,14 @@
+package chap02;
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = 10;
+		
+		int result = value + 10;
+		
+		System.out.print(result);
+	}
+
+}
