20cf1ab5c10c5a40c4de3e44a9b95a71ea7d2f1d
504 <504@DESKTOP-B0KVD9C>
2021-07-20 17:52:14 +0900

    chap07_8-수정

--- Java1/src/chap07_8/PhoneExample.java
+++ Java1/src/chap07_8/PhoneExample.java
@@ -3,7 +3,7 @@ package chap07_8;
 public class PhoneExample {
 
 	public static void main(String[] args) {
-		
+		//추상클래스는 new 연산자로 직접 생성자를 호출할 수 없다.
 		//Phone phone = new Phone();
 		
 		SmartPhone smartPhone = new SmartPhone("홍길동");
