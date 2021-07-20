ce8408ae6930f145a2407dcd0f0a16c7d55b9fed
504 <504@DESKTOP-B0KVD9C>
2021-07-20 15:55:12 +0900

    chap07_6-protect예제

---
+++ Java1/src/chap07_6/C.java
@@ -0,0 +1,11 @@
+package chap07_6;
+
+public class C {
+	
+	public void method() {
+		
+		A a = new A();          // (x)
+		a.field = "value";      // (x)
+		a.method();             // (x)
+	}
+}
---
+++ Java1/src/chap07_6/D.java
@@ -0,0 +1,12 @@
+package chap07_6;
+
+public class D extends A {
+
+	public D() {
+		
+		super();                // (o)
+		
+		this.field = "value";   // (o)
+		this.method();          // (o)
+	}
+}
