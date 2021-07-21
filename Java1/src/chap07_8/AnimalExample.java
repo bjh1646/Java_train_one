bc62919d5406fece422151debc481f9fce2a1ce6
byonk <byonk@192.168.200.101>
2021-07-21 17:51:58 +0900

    chap07_8-수정

---
+++ Java1/src/chap07_8/AnimalExample.java
@@ -0,0 +1,39 @@
+package chap07_8;
+
+public class AnimalExample {
+
+	public static void main(String[] args) {
+		
+		Dog dog = new Dog();
+		Cat cat = new Cat();
+		
+		dog.sound();
+		dog.breathe();
+		
+		cat.sound();
+		cat.breathe();
+		
+		System.out.println("----------");
+		
+		//변수의 자동 타입 변환
+		Animal animal = null;
+		
+		animal = new Dog();
+		animal.sound();
+		
+		animal = new Cat();
+		animal.sound();
+		
+		System.out.println("----------");
+		
+		//메소드의 다형성
+		animalSound(new Dog());
+		animalSound(new Cat());
+
+	}
+
+	private static void animalSound(Animal animal) {
+		animal.sound();
+	}
+	
+}
