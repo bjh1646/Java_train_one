33578a66cdc5da9e1b8bf4f650c65212bd857ef5
byonk <byonk@192.168.200.101>
2021-07-21 17:52:39 +0900

    chap10-완료

---
+++ Java1/src/chap10_7/AccountExample.java
@@ -0,0 +1,25 @@
+package chap10_7;
+
+public class AccountExample {
+
+	public static void main(String[] args) {
+		
+		Account account = new Account();
+		
+		//예금하기
+		account.deposit(10000);
+		System.out.println("예급액: " + account.getBalance());
+		
+		//출금하기
+		try {
+			account.withdraw(30000);
+		} catch (BalanceInsufficientException e) {
+			String message = e.getMessage();
+			System.out.println(message);
+			System.out.println();
+			e.printStackTrace();
+		}
+
+	}
+
+}
